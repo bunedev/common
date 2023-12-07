d70482e669f550ca8aaf077ed47a9a65cd381e41
devhoangkien <dev.kienhd@aneed.vn>
2023-12-07 15:34:39 +0700

    chore: fix logs error

--- all-exceptions.ts
+++ all-exceptions.ts
@@ -11,7 +11,7 @@ const append = promisify(appendFile);
 @Catch()
 export class GqlAllExceptionsFilter implements GqlExceptionFilter {
   private readonly logger = new Logger(GqlAllExceptionsFilter.name);
-  private logFilePath = path.join(__dirname, '../../../logs/error.log');
+  private logFilePath = path.join('logs/error.log');
 
   private async logToFile(message: string) {
     const logMessage = `${new Date().toISOString()} - ${message}\n`;
@@ -34,34 +34,29 @@ export class GqlAllExceptionsFilter implements GqlExceptionFilter {
     const gqlHost = GqlArgumentsHost.create(host);
     const context = gqlHost.getContext();
     const response = context?.res;
-
     let status = 500;
     let message = 'Internal Server Error';
     let messageCode = 0;
-    let code = 'INTERNAL_SERVER_ERROR';
     let stacktrace = [];
 
     if (exception instanceof GraphQLError) {
       message = exception.message;
-      code = exception.extensions?.code || ('INTERNAL_SERVER_ERROR' as any);
       stacktrace = exception.stack?.split('\n') || [];
       status = exception.extensions?.status || (500 as any);
       messageCode = exception.extensions?.messageCode as any;
     } else if (exception instanceof Error) {
       status = 'status' in exception ? exception['status'] : (500 as any);
       message = exception.message;
-      stacktrace = exception.stack?.split('\n') || [];
+      stacktrace = exception.stack?.split('\n') || [] as any;
     } else {
       this.logger.error('Unexpected error', exception);
     }
-
     if (response) {
       response.status(status).json({
         errors: [
           {
             message,
             extensions: {
-              code,
               messageCode,
               stacktrace,
             },
@@ -69,18 +64,9 @@ export class GqlAllExceptionsFilter implements GqlExceptionFilter {
         ],
         data: null,
       });
-    } else {
-      this.logger.error('HTTP response object is undefined', {
-        status,
-        message,
-        code,
-        messageCode,
-        stacktrace,
-        exception,
-      });
     }
 
-    const logOutput = `Error: ${message} Code: ${code} Status: ${status} messageCode: ${messageCode} stacktrace: ${stacktrace}`;
+    const logOutput = `Error: ${message} Status: ${status} messageCode: ${messageCode} stacktrace: ${stacktrace}`;
     this.logger.error(logOutput);
     this.logToFile(logOutput);
   }
