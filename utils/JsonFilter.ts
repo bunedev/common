eb70dd7af92bf68ea7bce633844c7a8ed6ce7f0c
devhoangkien <dev.kienhd@aneed.vn>
2023-12-25 17:35:43 +0700

    chore: remove swagger

--- utils/JsonFilter.ts
+++ utils/JsonFilter.ts
@@ -1,5 +1,4 @@
 import { Field, InputType } from "@nestjs/graphql";
-import { ApiProperty } from "@nestjs/swagger";
 import { IsOptional } from "class-validator";
 import { GraphQLJSONObject } from "graphql-type-json";
 
